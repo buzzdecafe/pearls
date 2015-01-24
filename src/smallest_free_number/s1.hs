1a12baebfbea97812714ad72a0497f7a4a49092d
buzzdecafe <m_hur@yahoo.com>
2015-01-24 10:38:01 -0500

    one down, 29 to go

--- src/smallest_free_number/s1.hs
+++ src/smallest_free_number/s1.hs
@@ -1,4 +1,5 @@
 import Data.Array
+import Data.List
 
 -- naive array-based solution 
 -- this example cribbed from derekmcloughlin/pearls to get started
@@ -7,11 +8,13 @@ import Data.Array
 minfree1 :: [Integer] -> Integer
 minfree1 xs = head([0..] \\ xs)
 
-(\\) :: Eq a => [a] -> [a] -> [a]
-us \\ vs = filter (notElem' vs) us
+-- using Data.List \\ definition here:
+--(\\) :: Eq a => [a] -> [a] -> [a]
+--us \\ vs = filter (notElem' vs) us
 
 notElem' :: Eq a => [a] -> a -> Bool
 notElem' a n = notElem n a
+
 
 -- find the first False solution
 -- iterates a list of Bools until it encounters a False, and returns the length of 
@@ -25,3 +28,20 @@ checklist xs = accumArray (||) False (0, n) (zip (filter (<= n) xs) (repeat True
 
 search :: Array Int Bool -> Int
 search = length . takeWhile id . elems
+
+
+-- divide and conquer solution
+minfrom :: Int -> (Int, [Int]) -> Int
+minfrom a (n, xs) | n == 0       = a
+                  | m == b - a   = minfrom b (n - m, vs)
+                  | otherwise    = minfrom a (m, us)
+                  where (us, vs) = partition (< b) xs
+                        b        = a + 1 + n `div` 2
+                        m        = length us
+
+minfree3 :: [Int] -> Int
+minfree3 xs = minfrom 0 (length xs, xs)
+
+-- [8, 23,  9, 0, 12, 11, 1, 10, 13, 7, 41, 4, 14, 21, 5, 17, 3, 19, 2, 6]
+-- minfree is `15`
+
